8cd26ce996887a4c8706963ae699c44f3550cf03
Jhon Hernandez <jhobahego16@hotmail.com>
2023-05-30 17:39:45 -0500

    Feat: First commit

---
+++ src/main/java/com/jhobadev/api_documentos/ApiDocumentosApplication.java
@@ -0,0 +1,13 @@
+package com.jhobadev.api_documentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDocumentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDocumentosApplication.class, args);
+	}
+
+}
