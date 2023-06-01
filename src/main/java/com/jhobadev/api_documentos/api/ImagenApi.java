55f526584da6cbac855091cf349606d0eee64567
Jhon Hernandez <jhobahego16@hotmail.com>
2023-05-31 21:31:03 -0500

    Feat: ruta para ver imagen

---
+++ src/main/java/com/jhobadev/api_documentos/api/ImagenApi.java
@@ -0,0 +1,43 @@
+package com.jhobadev.api_documentos.api;
+
+import com.jhobadev.api_documentos.servicio.DocumentoServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+
+@RestController
+public class ImagenApi {
+
+    DocumentoServicio documentoServicio;
+
+    @Autowired
+    public ImagenApi(DocumentoServicio documentoServicio) {
+        this.documentoServicio = documentoServicio;
+    }
+
+    @GetMapping("/imagenes/{imagen}")
+    public ResponseEntity<Resource> mostrarImagen(@PathVariable("imagen") String imagen) {
+        try {
+            Resource resource = documentoServicio.mostrarImagen(imagen);
+
+            boolean esJpgOPng = imagen.toLowerCase().endsWith(".jpg") || imagen.toLowerCase().endsWith(".jpeg");
+            String contentType = esJpgOPng ? MediaType.IMAGE_JPEG_VALUE
+                    : MediaType.IMAGE_PNG_VALUE;
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .body(resource);
+        } catch (MalformedURLException e) {
+            return ResponseEntity.notFound().build(); // Devuelve una respuesta 400 Bad Request en caso de URL inválida
+        }
+    }
+
+}
